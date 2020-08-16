fca902132d9ebfb087d380666792e2a4336f618c
семён дьяченко <cemchic05@outlook.com>
2020-08-16 14:58:37 +0300

    frontend more

---
+++ src/components/HowWorkContainerComponent/HowWorkContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HowWorkContainer.css';
+
+class HowWorkContainer extends React.Component {
+
+    render()
+    {
+        return(
+            <div className="tutorial-container container">
+                
+            </div>
+        );
+    }
+}
+
+export default HowWorkContainer;
